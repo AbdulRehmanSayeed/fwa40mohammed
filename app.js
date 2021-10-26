b63fdb329d3353385004191ae9bc4fb003fbdb53
AbdulRehmanSayeed <mohammed.abdulrehman1610@gmail.com>
2021-10-26 16:47:42 -0500

    new endpoint

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/mydata', mydataRouter)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
